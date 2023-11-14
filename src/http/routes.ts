112d4e4b7a2976d6454e764f4c0700e72058d615
Elias Oliveira <eliascpr123@gmail.com>
2023-11-14 16:22:34 -0300

    Initial commit

---
+++ src/http/routes.ts
@@ -0,0 +1,7 @@
+import { FastifyInstance } from 'fastify'
+
+export async function appRoutes(app: FastifyInstance) {
+  // app.post('/users', register)
+  /* Authenticated routes */
+  // app.get('/me', { onRequest: [verifyJWT] }, profile)
+}
